fefb04862952616a3aa46420857e9e6aecdf513b
mepotts <mepotts@ischool.berkeley.edu>
2016-09-20 15:23:23 -0400

    Add files via upload

---
+++ machinelearning/kaggle/titanic/titanicGLM.R
@@ -0,0 +1,70 @@
+library(caret)
+library(randomForest)
+library(fields)
+
+setwd("C:/Users/mpotts/Dropbox/UC Berkeley/Machine Learning/Kaggle Competitions/Titanic_Machine_Learning_from_Disaster")
+
+trainSet <- read.csv("train2.csv", header = TRUE)
+testSet <- read.table("test2.csv", sep = ",", header = TRUE)
+
+# Comparing variables and survived.
+
+fields::bplot.xy(trainSet$Survived, trainSet$Age)
+
+table(trainSet[,c("Survived", "Pclass")])
+
+
+# Convert Survived to Factor
+trainSet$Survived <- factor(trainSet$Survived)
+trainSet$Pclass <- factor(trainSet$Pclass)
+testSet$Pclass <- factor(testSet$Pclass)
+
+# Remove NAs and replace with the mean. 
+trainSet$Fare <- ifelse(is.na(trainSet$Fare), mean(trainSet$Fare, na.rm = TRUE), trainSet$Fare)
+trainSet$Age <- ifelse(is.na(trainSet$Age), mean(trainSet$Age, na.rm = TRUE), trainSet$Age)
+
+
+# Set a random seed (so you will get the same results as me)
+set.seed(42)
+# Train the model using a "random forest" algorithm
+
+lgrs <- glm(Survived ~ Pclass + Sex + Age + SibSp + Embarked + Pclass:Sex + Pclass:Age
+            , data = trainSet, family = binomial(logit))
+
+lgrs
+
+stepb <- step(lgrs, direction = "backward")
+stepf <- step(lgrs, direction = "forward")
+stepw <- step(lgrs, direction = "both")
+
+stepb$anova
+stepf$anova
+stepw$anova
+
+trainSet$pr <- as.vector(ifelse(predict(lgrs, type = "response", trainSet) > 0.6, "1", "0"))
+table(trainSet$Survived, trainSet$pr, dnn = c("Actual", "Predicted"))
+
+###############ROC Curve##################################
+
+library(ROCR)
+fitpreds = predict(lgrs,newdata=trainSet,type="response");
+fitpred = prediction(fitpreds,trainSet$Survived);
+fitperf = performance(fitpred,"tpr","fpr");
+plot(fitperf,col="green",lwd=2,main="ROC Curve for Logistic regression")
+abline(a=0,b=1,lwd=2,lty=2,col="gray")
+summary(lgrs);
+
+
+# Remove NAs from trainSet and replace with the mean. 
+testSet$Fare <- ifelse(is.na(testSet$Fare), mean(testSet$Fare, na.rm = TRUE), testSet$Fare)
+testSet$Age <- ifelse(is.na(testSet$Age), mean(testSet$Age, na.rm = TRUE), testSet$Age)
+
+# add the predictions to the testSet
+testSet$Survived <- as.vector(ifelse(predict(lgrs, type = "response", testSet) > 0.6, "1", "0"))
+
+# view predictions
+predictions <- as.vector(ifelse(predict(lgrs, type = "response", testSet) > 0.6, "1", "0"))
+predictions
+
+submission <- testSet[, c("PassengerId", "Survived")]
+write.table(submission, file = "submission.csv", col.names = TRUE, row.names = FALSE, sep = ",")
---
+++ machinelearning/kaggle/titanic/titanicSVM.R
@@ -0,0 +1,54 @@
+library(caret)
+library(kernlab)
+library(randomForest)
+library(fields)
+
+setwd("C:/Users/mpotts/Dropbox/UC Berkeley/Machine Learning/Kaggle Competitions/Titanic_Machine_Learning_from_Disaster")
+
+trainSet <- read.table("train2.csv", sep = ",", header = TRUE)
+testSet <- read.table("test2.csv", sep = ",", header = TRUE)
+
+# Comparing variables and survived.
+
+fields::bplot.xy(trainSet$Survived, trainSet$Age)
+
+table(trainSet[,c("Survived", "Pclass")])
+
+
+# Convert Survived to Factor
+trainSet$Survived <- factor(trainSet$Survived)
+
+# Set a random seed (so you will get the same results as me)
+set.seed(42)
+
+# Train the model using a "svm" algorithm
+model1 <- train(Survived ~ Pclass + Sex + Age + SibSp + Parch + Embarked + Fare + Pclass:Sex + Pclass:Age + Age:Sex, # Survived is a function of the variables we decided to include
+               data = trainSet, # Use the trainSet dataframe as the training data
+               method = "svmRadial",# Algorithm used
+               metric = "Accuracy",
+               preProc = c("knnImpute", "center", "scale"),
+               trControl = trainControl(method = "repeatedcv", # Use cross-validation
+                                        number = 5), # Use 5 folds for cross-validation
+               tuneLength = 10
+)
+
+model1
+
+# Remove NAs and replace with the mean.
+testSet$Fare <- ifelse(is.na(testSet$Fare), mean(testSet$Fare, na.rm = TRUE), testSet$Fare)
+testSet$Age <- ifelse(is.na(testSet$Age), mean(testSet$Age, na.rm = TRUE), testSet$Age)
+
+# Caret SVM model to predict and create a submission file
+testSet$Survived <- predict(model1, newdata = testSet)
+
+predictions <- predict(model1, newdata = testSet)
+predictions
+
+write.table(testSet[, c("PassengerId", "Survived")], file = "submission4.csv", col.names = TRUE, row.names = FALSE, sep = ",")
+
+
+
+
+
+
+
